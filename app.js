81c1017377bb0c1aef65b6c8d665cfbedf1e1dc9
Srivignesh Sivakumar <shreez77890@gmail.com>
2022-08-15 15:38:24 -0500

    operators commit

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
+let a = 20;
+let b = 4;
+let add = a+b;
+let minus = a-b;
+let multiply = a*b;
+let dividing = a/b;
 
-// YOUR CODE HERE
-
+console.log(a, b , add, minus, multiply, dividing);
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(num + str, num + str2, num + isPresent, isPresent + str, firstName + lastName);
 
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
 
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent = false;
+
+ console.log (val == str3);
+console.log (val === str3);
+console.log (!isPresent);
+console.log (“eleven” == str4 && val >= str3);
+ console.log (value0 == false);
+console.log (0 === false);
+console.log (0 != false);
+console.log (0 !== false);
 // YOUR CODE HERE
